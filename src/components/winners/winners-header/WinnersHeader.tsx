10c235fc9c56b103afa896079bdfbe2bde974ba0
Valery91-qw <vakriv91@gmail.com>
2022-03-18 10:08:59 +0300

    feat: add display current page

--- src/components/winners/winners-header/WinnersHeader.tsx
+++ src/components/winners/winners-header/WinnersHeader.tsx
@@ -5,10 +5,14 @@ import {RootStoreType} from "../../../bll/store";
 
 const WinnersHeader = () => {
     const totalWinners = useSelector<RootStoreType, number>(state => state.reducer.winnerTotalCount);
+    const currentPage = useSelector<RootStoreType, number>(state => state.reducer.winnerPage);
 
-    return (<div className={style.wrapper}>
-        <h3 className={style.title}>Winners total: {totalWinners}</h3>
-    </div>)
+    return (
+        <div className={style.wrapper}>
+            <h3 className={style.title}>Winners total: {totalWinners}</h3>
+            <h3 className={style.title}>Page: {currentPage}</h3>
+        </div>
+    )
 };
 
 export default WinnersHeader
